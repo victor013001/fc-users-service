064a850a5b3ded6ffe47c57b5f208985e402d61b
victor013001 <victormanuelosoriog@gmail.com>
2025-06-05 18:01:49 -0500

    archetype base

---
+++ src/main/java/com/example/non_reactive_archetype/NonReactiveArchetypeApplication.java
@@ -0,0 +1,13 @@
+package com.example.non_reactive_archetype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonReactiveArchetypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonReactiveArchetypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/non_reactive_archetype/NonReactiveArchetypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.non_reactive_archetype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonReactiveArchetypeApplicationTests {
+
+  @Test
+  void contextLoads() {
+    // This method is intentionally left empty to verify that the Spring application context loads
+  }
+}
